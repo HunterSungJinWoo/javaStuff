6ff3328f7fd903e9219cd1467c0dd32a191ff002
Sung <60687264+HunterSungJinWoo@users.noreply.github.com>
2020-11-30 08:09:29 +0100

    Upload Not finished work

---
+++ 30.11.2020/TaskCLISS.js
@@ -0,0 +1,21 @@
+/*
+
+Create an HTML page with two inputs, one select box and one button.
+
+When the user clicks on the button, we need to display on the HTML page a "matrix" (a grid) of boxes with 
+background colors.
+
+We need to display as many rows as specified by the first input box.
+The number of columns in the grid is given by the second input box.
+The background color of the boxes should be specified by the select -> choose between three different possible 
+colors.
+
+To make this easier, I will provide the HTML code.
+
+
+*/
+
+function CreateGrid()
+{
+    // put your code here !!
+}
