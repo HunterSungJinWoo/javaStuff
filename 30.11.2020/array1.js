bf2a3fc7273481670b4d3184f00c8099caae314d
Sung <60687264+HunterSungJinWoo@users.noreply.github.com>
2020-11-30 10:49:16 +0100

    Array done

--- 30.11.2020/array1.js
+++ 30.11.2020/array1.js
@@ -1,9 +1,7 @@
-
+var arrayOfCities = ["Luxembourg city", "Esch","Wiltz","Junglinster","Wormeldange","bla","Yet another city","aaa"];
 
 function DoDisplay()
 {
-    var arrayOfCities = ["Luxembourg city", "Esch","Wiltz","Junglinster","Wormeldange","bla","Yet another city","aaa"];
-
 
     document.getElementById("Where").innerHTML = "";
     for(let i=0;i<arrayOfCities.length;i++)
@@ -12,4 +10,32 @@ function DoDisplay()
         document.getElementById("Where").innerHTML += cityNumber + " -" + arrayOfCities[i] + "<BR>";
     }    
 
+}
+
+var arrayOfCities = ["Luxembourg city", "Esch","Junglinster"];
+
+function DoDisplayReversed()
+{
+
+    document.getElementById("Where").innerHTML = "";
+    for(let i=arrayOfCities.length-1;i>=0;i--)
+    {
+        let cityNumber = i+1;
+        document.getElementById("Where").innerHTML += cityNumber + " -" + arrayOfCities[i] + "<BR>";
+
+    } 
+
+}
+
+function AddCityToArray()
+{
+    let myNEwCity = document.getElementById("CityName").value;
+    arrayOfCities.push(myNEwCity);
+    DoDisplay();
+}
+
+function DeleteLastCity()
+{
+    arrayOfCities.pop();
+    DoDisplay();
 }
